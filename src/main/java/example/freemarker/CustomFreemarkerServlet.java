84b72c6bc2a5687e059317d91d03679354106f21
tomcz <github@tomczarniecki.com>
2011-10-29 00:02:26 +1100

    still log decent context from freemarker template errors

--- src/main/java/example/freemarker/CustomFreemarkerServlet.java
+++ src/main/java/example/freemarker/CustomFreemarkerServlet.java
@@ -2,11 +2,11 @@ package example.freemarker;
 
 import freemarker.cache.TemplateLoader;
 import freemarker.ext.servlet.FreemarkerServlet;
-import freemarker.template.Configuration;
 import freemarker.template.SimpleHash;
 import freemarker.template.Template;
 import freemarker.template.TemplateModel;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -14,22 +14,21 @@ import java.io.IOException;
 public class CustomFreemarkerServlet extends FreemarkerServlet {
 
     @Override
-    protected boolean preTemplateProcess(HttpServletRequest request, HttpServletResponse response,
-                                         Template template, TemplateModel data) {
-
-        ((SimpleHash) data).put("base", request.getContextPath());
-        return true;
-    }
-
-    @Override
-    protected Configuration createConfiguration() {
-        Configuration configuration = super.createConfiguration();
-        configuration.setTemplateExceptionHandler(new HtmlExceptionHandler());
-        return configuration;
+    public void init() throws ServletException {
+        super.init(); // allow freemarker servlet to setup its configuration
+        getConfiguration().setTemplateExceptionHandler(new HtmlExceptionHandler());
     }
 
     @Override
     protected TemplateLoader createTemplateLoader(String templatePath) throws IOException {
         return new HtmlTemplateLoader(super.createTemplateLoader(templatePath));
+    }
+
+    @Override
+    protected boolean preTemplateProcess(HttpServletRequest request, HttpServletResponse response,
+                                         Template template, TemplateModel data) {
+
+        ((SimpleHash) data).put("base", request.getContextPath());
+        return true;
     }
 }
